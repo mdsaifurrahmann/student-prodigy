6a7c379d3803ead9518bcc3886144aa19b5e9c15
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2022-10-04 21:51:09 +0600

    workspace updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -72,7 +72,6 @@ mix
   .js('resources/js/core/app.js', 'public/js/core')
   .js('resources/assets/js/scripts.js', 'public/js/core')
   .js('resources/assets/js/dropdowns.js', 'public/js/core')
-  .js('resources/assets/js/alpine.js', 'public/js/core')
   .sass('resources/scss/base/themes/dark-layout.scss', 'public/css/base/themes', { sassOptions })
   .sass('resources/scss/base/themes/bordered-layout.scss', 'public/css/base/themes', { sassOptions })
   .sass('resources/scss/base/themes/semi-dark-layout.scss', 'public/css/base/themes', { sassOptions })
