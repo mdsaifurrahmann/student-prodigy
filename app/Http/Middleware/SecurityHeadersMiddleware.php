e424fb3c6c07af73dc52413177250bef4db8f604
mdsaifurrahmann <md.saifurrahmann029@gmail.com>
2023-04-09 01:45:46 +0600

    security headers bug fix

--- app/Http/Middleware/SecurityHeadersMiddleware.php
+++ app/Http/Middleware/SecurityHeadersMiddleware.php
@@ -7,26 +7,26 @@ use Illuminate\Http\Request;
 
 class SecurityHeadersMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
+   /**
+    * Handle an incoming request.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+    * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+    */
    public function handle($request, Closure $next)
    {
       $response = $next($request);
 
-      $response->header('X-Content-Type-Options', 'nosniff');
-      $response->header('X-Download-Options', 'noopen');
-      $response->header('X-XSS-Protection', '1; mode=block');
-      $response->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
-      $response->header('X-Frame-Options', 'SAMEORIGIN');
-      $response->header('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: www.w3.org; font-src 'self' fonts.gstatic.com; connect-src 'self'; frame-ancestors 'none'; style-src-elem 'self' fonts.googleapis.com;");
+      $response->headers->set('X-Content-Type-Options', 'nosniff');
+      $response->headers->set('X-Download-Options', 'noopen');
+      $response->headers->set('X-XSS-Protection', '1; mode=block');
+      $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
+      $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
+      $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: www.w3.org; font-src 'self' fonts.gstatic.com; connect-src 'self'; frame-ancestors 'none'; style-src-elem 'self' fonts.googleapis.com;");
 
-      $response->header('Referrer-Policy', 'no-referrer-when-downgrade');
-      $response->header('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+      $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
+      $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
 
       // Remove headers
       $response->headers->remove('X-Powered-By');
@@ -35,5 +35,4 @@ class SecurityHeadersMiddleware
 
       return $response;
    }
-
 }
